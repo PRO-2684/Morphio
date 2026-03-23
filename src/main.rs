be30d30b7e7400d42cc7e13a8618e31016f98d89
PRO-2684 <54608551+PRO-2684@users.noreply.github.com>
2026-03-23 20:59:10 +0800

    feat: Cli

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,42 @@
-use read_fonts::FontRef;
-use std::fs::{read, write};
+use argh::FromArgs;
+use read_fonts::FileRef;
+use std::{
+    fs::{read, write},
+    path::PathBuf,
+};
 use text_morph::TextMorph;
 
+/// Morphio: Morphs the font, so it shows worda as wordb.
+#[derive(FromArgs)]
+#[argh(help_triggers("-h", "--help"))]
+struct Args {
+    /// word to morph from
+    #[argh(positional)]
+    from: String,
+    /// word to morph to
+    #[argh(positional)]
+    to: String,
+    /// input font file path
+    #[argh(option, short = 'i')]
+    input: PathBuf,
+    /// output font file path
+    #[argh(option, short = 'o')]
+    output: PathBuf,
+    /// allow overwrite output file if it exists
+    #[argh(switch, short = 'y')]
+    yes: bool,
+}
+
 fn main() {
-    let data = read("./tests/fonts/msyh.ttc").unwrap();
-    let font = FontRef::from_index(&data, 0).unwrap();
-    let morphed = font.morph("banana", "orange").unwrap();
-    write("./tests/fonts/msyh-morphed.ttc", morphed).unwrap();
+    let args: Args = argh::from_env();
+    if args.output.exists() && !args.yes {
+        eprintln!("Output file already exists. Use -y to overwrite.");
+        std::process::exit(1);
+    }
+    let data = read(&args.input).expect("Failed to read input font file");
+    let font = FileRef::new(&data).expect("Failed to parse font file");
+    let morphed = font
+        .morph(&args.from, &args.to)
+        .expect("Failed to morph font");
+    write(&args.output, morphed).expect("Failed to write output font file");
 }
