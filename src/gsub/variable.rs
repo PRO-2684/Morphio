2507c0fac6750e7ac3663ce36a54e6586389a5b0
PRO-2684 <54608551+PRO-2684@users.noreply.github.com>
2026-03-25 21:03:15 +0800

    fix: many-to-one not working

--- src/gsub/variable.rs
+++ src/gsub/variable.rs
@@ -18,40 +18,58 @@ pub fn append_variable_length_lookups(
     placeholder: GlyphId16,
     word_glyph_ranges: Vec<RangeRecord>,
 ) -> Result<Vec<u16>, MorphError> {
-    let mut lookup_indices = Vec::new();
-
-    let expand_input = if matches!(from_glyphs, [_]) {
-        from_glyphs
-    } else {
-        let collapse_lookup = create_ligature_substitution_lookup(from_glyphs, placeholder);
+    if matches!(from_glyphs, [_, ..]) && matches!(to_glyphs, [_]) {
+        let collapse_lookup = create_ligature_substitution_lookup(from_glyphs, to_glyphs[0]);
         let collapse_lookup_index = push_lookup(gsub, collapse_lookup)?;
         let collapse_context_index = push_lookup(
             gsub,
             create_contextual_lookup(
                 from_glyphs,
-                word_glyph_ranges.clone(),
+                word_glyph_ranges,
                 vec![SequenceLookupRecord::new(0, collapse_lookup_index)],
             ),
         )?;
-        lookup_indices.push(collapse_context_index);
-        std::slice::from_ref(&placeholder)
-    };
+        return Ok(vec![collapse_context_index]);
+    }
 
-    if !matches!(to_glyphs, [_]) {
-        let expand_lookup = create_multiple_substitution_lookup(expand_input[0], to_glyphs);
+    if matches!(from_glyphs, [_]) {
+        let expand_lookup = create_multiple_substitution_lookup(from_glyphs[0], to_glyphs);
         let expand_lookup_index = push_lookup(gsub, expand_lookup)?;
         let expand_context_index = push_lookup(
             gsub,
             create_contextual_lookup(
-                expand_input,
+                from_glyphs,
                 word_glyph_ranges,
                 vec![SequenceLookupRecord::new(0, expand_lookup_index)],
             ),
         )?;
-        lookup_indices.push(expand_context_index);
+        return Ok(vec![expand_context_index]);
     }
 
-    Ok(lookup_indices)
+    let collapse_lookup = create_ligature_substitution_lookup(from_glyphs, placeholder);
+    let collapse_lookup_index = push_lookup(gsub, collapse_lookup)?;
+    let collapse_context_index = push_lookup(
+        gsub,
+        create_contextual_lookup(
+            from_glyphs,
+            word_glyph_ranges.clone(),
+            vec![SequenceLookupRecord::new(0, collapse_lookup_index)],
+        ),
+    )?;
+
+    let expand_input = std::slice::from_ref(&placeholder);
+    let expand_lookup = create_multiple_substitution_lookup(placeholder, to_glyphs);
+    let expand_lookup_index = push_lookup(gsub, expand_lookup)?;
+    let expand_context_index = push_lookup(
+        gsub,
+        create_contextual_lookup(
+            expand_input,
+            word_glyph_ranges,
+            vec![SequenceLookupRecord::new(0, expand_lookup_index)],
+        ),
+    )?;
+
+    Ok(vec![collapse_context_index, expand_context_index])
 }
 
 fn create_multiple_substitution_lookup(src: GlyphId16, dst: &[GlyphId16]) -> SubstitutionLookup {
